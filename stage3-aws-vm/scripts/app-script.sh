3b0fc25bde707cfc5b37efb12e7b4783d17f8b96
Alina Omar <alinaomar13@gmail.com>
2026-01-15 09:31:31 +0000

    fixed some errors

--- stage3-aws-vm/scripts/app-script.sh
+++ stage3-aws-vm/scripts/app-script.sh
@@ -20,18 +20,27 @@ echo "Install Maven"
 sudo DEBIAN_FRONTEND=noninteractive apt-get install maven -y
 echo "Done!"
 
+echo "Install MySQL client for testing..."
+sudo apt install mysql-client-core-8.0 -y
+echo "Done!"
+
 echo "Clone GitHub repository..."
 cd /home/ubuntu
 git clone https://github.com/Alina858/library-java-deployment.git
 echo "Done!"
 
+echo "Set ownership of cloned repo..."
+sudo chown -R ubuntu:ubuntu /home/ubuntu/library-java-deployment
+echo "Done!"
+
 echo "Set Enviroment variables"
-export DB_HOST="jdbc:mysql://localhost:3306/library"
+export DB_HOST="jdbc:mysql://"Public-Ip":3306/library"
 export DB_USER= "appuser"
 export DB_PASS= "SecurePass123!"
+echo "Done!"
 
 echo "Navigate to app folder and run..."
-cd library-java-deployment/ProjectLibrary2
+cd library-java-deployment/LibraryProject2
 mvn spring-boot:run
 
 
